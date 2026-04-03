b026be9e6ee94ab55e1d7407510512daafd273d0
Alexander <astelmah060@gmail.com>
2026-04-03 18:55:57 +0300

    Update .gitignore and add favicon link in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="icon" href="{{ asset('img/favicon.png') }}">
 
 </head>
 <body>
